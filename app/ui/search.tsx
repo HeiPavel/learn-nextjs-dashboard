65df61fa09cb8a4bedc761a7c99c69c9e5e023f8
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2024-01-29 01:33:18 +0200

    Implement debouncing with use-debounce package

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,33 +2,18 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
-import { useRef } from 'react';
-
-type Timeout = {
-  current: ReturnType<typeof setTimeout> | number
-}
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const {replace} = useRouter();
-  const timeoutID: Timeout = useRef(0);
 
-  /*function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     term ? params.set('query', term) : params.delete('query');
     replace(`${pathname}?${params.toString()}`);
-  }*/
-
-
-  function handleSearch(term: string) {
-    clearTimeout(timeoutID.current);
-    timeoutID.current = setTimeout(() => {
-      const params = new URLSearchParams(searchParams);
-      term ? params.set('query', term) : params.delete('query');
-      replace(`${pathname}?${params.toString()}`);
-    }, 300);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
